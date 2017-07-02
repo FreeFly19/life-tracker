cd55ca36a77965988642d77b527d99296a6dbc6f
alexandrm <oleksandrm@Interlink-ua.com>
2017-07-02 12:00:54 +0300

    Added kibana

--- src/main/scala/TestEl.scala
+++ src/main/scala/TestEl.scala
@@ -4,39 +4,41 @@ import java.time.LocalDateTime
 import com.sksamuel.elastic4s.ElasticsearchClientUri
 import com.sksamuel.elastic4s.http.HttpClient
 
+import scala.util.Random
+
 object TestEl extends App {
   import com.sksamuel.elastic4s.http.ElasticDsl._
 
   val client = HttpClient(ElasticsearchClientUri("localhost", 9200))
 
-  client.execute(deleteIndex("life-tracker")).await
-
-  client.execute{
-    createIndex("life-tracker").mappings(
-      mapping("span").fields(
-        textField("title"),
-        textField("description"),
-        objectField("at").fields (
-          dateField("start"),
-          dateField("finish")
-        ),
-        keywordField("tags")
-      )
-    )
-  }.await
+//  client.execute(deleteIndex("life-tracker")).await
+//
+//  client.execute{
+//    createIndex("life-tracker").mappings(
+//      mapping("span").fields(
+//        textField("title"),
+//        textField("description"),
+//        objectField("at").fields (
+//          dateField("start"),
+//          dateField("finish")
+//        ),
+//        keywordField("tags")
+//      )
+//    )
+//  }.await
 
   val startAt = System.currentTimeMillis
 
   var time = Timestamp.valueOf(LocalDateTime.now).getTime
 
-  val docs = for(i <- 1 until 1000000) yield indexInto("life-tracker" / "span") fields(
+  val docs = for(i <- 1 until 10000) yield indexInto("life-tracker" / "span") fields(
       "title" -> ("TextItem" + i),
       "description" -> "Some desc",
       "at" -> Map(
         "start" -> time,
         "finish" -> (time + (math.random()*10000).toLong + i * 10000)
       ),
-      "tags" -> "work"
+      "tags" -> (if(Random.nextBoolean()) "work" else "home")
     )
 
   docs.grouped(10000).foreach(g => client.execute(bulk(g)).await)
@@ -46,10 +48,10 @@ object TestEl extends App {
   client.close
 
 
-  val res = client.execute{
-    search("life-tracker" / "span") query "textitem1"
-  }.await
-
-  println(res.hits.hits.head.sourceAsString)
+//  val res = client.execute{
+//    search("life-tracker" / "span") query "textitem1"
+//  }.await
+//
+//  println(res.hits.hits.head.sourceAsString)
 }
 
